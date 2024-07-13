3f003d98492b051ca8a0b8ca0781f577523d99e1
choam2426 <choam2426@gmail.com>
2024-07-13 20:29:37 +0900

    bugfix name miss

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from src.consts import *
-from src.use_github_api import get_commit_by_sha
+from src.use_github_api import get_commit_data
 
 if __name__ == "__main__":
-    get_commit_by_sha(GITHUB_REPOSITORY, COMMIT_SHA, GITHUB_TOKEN)
+    get_commit_data(GITHUB_REPOSITORY, COMMIT_SHA, GITHUB_TOKEN)
