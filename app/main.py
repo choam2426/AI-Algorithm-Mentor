ddc0ef4dcc81dd3e703dfd456df289d8a2a80d7f
choam2426 <choam2426@gmail.com>
2024-07-13 21:38:15 +0900

    test case 1

--- app/main.py
+++ app/main.py
@@ -2,5 +2,7 @@ from src import *
 
 if __name__ == "__main__":
     diffs = get_commit_data(GITHUB_REPOSITORY, COMMIT_SHA, GITHUB_TOKEN)
-    write_comment_in_commit(repository=GITHUB_REPOSITORY, commit_sha=COMMIT_SHA, token=GITHUB_TOKEN, comment="test")
-    # llm_response = get_code_review_by_openai(api_key=OPENAI_API_KEY, diffs=diffs)
+
+    llm_response = get_code_review_by_openai(api_key=OPENAI_API_KEY, diffs=diffs)
+
+    write_comment_in_commit(repository=GITHUB_REPOSITORY, commit_sha=COMMIT_SHA, token=GITHUB_TOKEN, comment=llm_response)
