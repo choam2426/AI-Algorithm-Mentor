721b135f8574d3b5b6de6ebbcd40def690f84123
choam2426 <choam2426@gmail.com>
2024-07-27 20:46:33 +0900

    bugfix : wrong parameter

--- app/main.py
+++ app/main.py
@@ -3,6 +3,6 @@ from src import *
 if __name__ == "__main__":
     file_contents = get_commit_data(GITHUB_REPOSITORY, COMMIT_SHA, GITHUB_TOKEN)
 
-    llm_response = get_code_review_by_openai(api_key=OPENAI_API_KEY, diffs=file_contents)
+    llm_response = get_code_review_by_openai(api_key=OPENAI_API_KEY, file_contents=file_contents)
 
     write_comment_in_commit(repository=GITHUB_REPOSITORY, commit_sha=COMMIT_SHA, token=GITHUB_TOKEN, comment=llm_response)
