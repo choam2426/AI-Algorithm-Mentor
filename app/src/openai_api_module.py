2f826e6ec41d0594b4599a396eb2a66439227892
choam2426 <choam2426@gmail.com>
2024-09-21 17:11:15 +0900

    Update prompt

--- app/src/openai_api_module.py
+++ app/src/openai_api_module.py
@@ -10,8 +10,8 @@ def get_code_review_by_openai(api_key: str, file_contents: dict):
     prompt = get_prompt(file_contents)
     completion = client.chat.completions.create(
         model="gpt-4o",
-        temperature=0.3,
-        top_p=0.2,
+        temperature=0.2,
+        top_p=1.0,
         frequency_penalty=0,
         presence_penalty=0,
         messages=prompt,
